14dca1ef5e75f1e79329467d78dcdea7cbb920be
Nils Ceberg <nils.ceberg@gmail.com>
2025-06-24 17:42:31 +0200

    parameters styling

--- src/Parameters.tsx
+++ src/Parameters.tsx
@@ -5,7 +5,7 @@ import { action } from "mobx";
 
 const Input = (props: PropsWithChildren<{ label: string }>) => {
     return <div className="flex flex-row m-2 h-[30px]">
-        <div className="basis-0 grow-1 whitespace-nowrap overflow-hidden overflow-ellipsis text-right content-center h-full pr-4">{props.label}: </div>
+        <div className="basis-0 grow-1 whitespace-nowrap overflow-hidden overflow-ellipsis text-left content-center h-full pr-4">{props.label}: </div>
         {props.children}
     </div>;
 };
@@ -16,6 +16,10 @@ const Numeric = (props: { value: number, min: number, max: number, onChange: (va
 
 const Slider = (props: { value: number, min: number, max: number, onChange: (value: any) => void }) => {
     return <input className="basis-0 grow-[3]" type="range" value={props.value} onChange={e => props.onChange(parseFloat(e.target.value) || 0)} max={props.max} min={props.min} step={(props.max - props.min) / 100}/>;
+}
+
+const Divider = (props: PropsWithChildren<{}>) => {
+    return <div className="w-full ml-auto mr-auto text-center border-b-1 border-[#777] text-[#ccc] mb-2 mt-3">{props.children}</div>
 }
 
 export interface InputParameters {
@@ -32,24 +36,29 @@ interface Props {
 
 export const Parameters = observer((props: Props) => {
     return (
-        <div className="p-10 pt-20">
+        <div className="p-10 pt-10">
             {/*<button onClick={action(() => props.state.activity = randomActivity(props.state.neurons))}>Randomize activity</button>*/}
-            <Input label="Time constant">
+            <Divider>Simulation</Divider>
+            <Input label="Neuronal time constant">
                 <Numeric min={0.01} max={100} value={props.state.time_constant} onChange={action(value => props.state.time_constant = value)}/>
                 <Slider min={-2} max={2} value={Math.log10(props.state.time_constant)} onChange={action(value => props.state.time_constant = Math.pow(10, value))}/>
             </Input>
-            <Input label="Noise">
+            <Input label="Neuronal noise">
                 <Numeric min={0.01} max={10} value={props.state.volatility} onChange={action(value => props.state.volatility = value)}/>
                 <Slider min={-2} max={1} value={Math.log10(props.state.volatility)} onChange={action(value => props.state.volatility = Math.pow(10, value))}/>
             </Input>
+
+            <Divider>Stimulus</Divider>
             <Input label="Compass angle">
                 <Numeric min={-Math.PI} max={Math.PI} value={props.inputs.angle} onChange={action(value => props.inputs.angle = value)}/>
                 <Slider min={-Math.PI} max={Math.PI} value={props.inputs.angle} onChange={action(value => props.inputs.angle = value)}/>
             </Input>
-            <Input label="Compass strength">
+            <Input label="Signal strength">
                 <Numeric min={0} max={1} value={props.inputs.strength} onChange={action(value => props.inputs.strength = value)}/>
                 <Slider min={0} max={1} value={props.inputs.strength} onChange={action(value => props.inputs.strength = value)}/>
             </Input>
+
+            <Divider>Connectivity</Divider>
             <Input label="a">
                 <Numeric min={-1} max={1} value={props.inputs.a} onChange={action(value => props.inputs.a = value)}/>
                 <Slider min={-1} max={1} value={props.inputs.a} onChange={action(value => props.inputs.a = value)}/>
