4091e590e1c965d4c6d586a19c7ce91ec2314806
Nils Ceberg <nils.ceberg@gmail.com>
2025-09-26 15:19:08 +0200

    remove top margin from firstr divider

--- src/ui.tsx
+++ src/ui.tsx
@@ -1,5 +1,5 @@
 import { PropsWithChildren } from "react"
 
 export const Divider = (props: PropsWithChildren<{}>) => {
-    return <div className="w-full grow-0 basis-0 ml-auto mr-auto text-center border-b-1 border-[#777] text-[#ccc] mb-2 mt-3">{props.children}</div>
+    return <div className="w-full grow-0 basis-0 ml-auto mr-auto text-center border-b-1 border-[#777] text-[#ccc] mb-2 not-first:mt-3">{props.children}</div>
 }
