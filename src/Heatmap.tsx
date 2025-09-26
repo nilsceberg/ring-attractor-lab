8d2ff3fa5751bd3a2efcadb6438f3911b98091a2
Nils Ceberg <nils.ceberg@gmail.com>
2025-09-26 22:54:15 +0200

    heat map optimization

--- src/Heatmap.tsx
+++ src/Heatmap.tsx
@@ -3,50 +3,38 @@ import { HEATMAP_COLOR } from "./colors";
 import { SimulationState } from "./simulation";
 import { MAX_HISTORY_SAMPLES } from "./settings";
 import { HistoryEntry } from "./Plots";
+import { useRef } from "react";
 
 interface HeatmapData {
     completedChunks: string[],
+    chunkWidth: number,
+    head: PNG,
+    headColumn: number,
+    time: number,
 }
 
-function createDebugChunk(height: number, width: number): string {
+function createEmptyChunk(height: number, width: number): string {
     let png = new PNG({
         width,
         height,
     });
-
-    for (let i=0; i<width; ++i) {
-        const color = 0xFF0000;
-        for (let j=0; j < height; ++j) {
-            png.data.writeUInt32BE((color << 8) + Math.round(i / width), 4*(width * ((height - j - 1 + height / 2) % height) + i));
-        }
-    }
-    
     return PNG.sync.write(png).toString("base64");
 }
 
 function initializeHeatmap(chunkCount: number, totalWidth: number, height: number): HeatmapData {
-    const chunks = [];
+    const completedChunks = [];
     const chunkWidth = Math.ceil(totalWidth / chunkCount);
-    for (let i=0; i<=chunkCount; ++i) {
-        chunks.push(createDebugChunk(height, 64));
+    for (let i=0; i<chunkCount; ++i) {
+        completedChunks.push(createEmptyChunk(height, chunkWidth));
     }
     return {
-        completedChunks: chunks,
+        completedChunks,
+        chunkWidth,
+        head: new PNG({ width: chunkWidth, height }),
+        headColumn: 0,
+        time: 0,
     }
 }
-
-
-    //if (legend.heatmap) {
-    //    for (let i=0; i<props.history.length; ++i) {
-    //        const activity = props.history[props.history.length - i - 1].activity;
-    //        const k = MAX_HISTORY_SAMPLES - i - 1;
-    //        for (let j=0; j < activity.length; ++j) {
-    //            png.data.writeUInt32BE((HEATMAP_COLOR << 8) + Math.round(activity[j] * 255), 4*(MAX_HISTORY_SAMPLES * ((num_neurons - j - 1 + num_neurons / 2) % num_neurons) + k))
-    //        }
-    //    }
-    //}
-    //const img = PNG.sync.write(png).toString("base64");
-
 
 interface HeatmapProps {
     state: SimulationState,
@@ -56,22 +44,73 @@ interface HeatmapProps {
     yExtent: [number, number],
 }
 
+const CHUNK_COUNT = 4;
 export const Heatmap = (props: HeatmapProps) => {
-    const num_neurons = props.state.neurons;
-    let png = new PNG({
-        width: MAX_HISTORY_SAMPLES,
-        height: num_neurons,
-    });
-    if (props.show) {
-        for (let i=0; i<props.history.length; ++i) {
-            const activity = props.history[props.history.length - i - 1].activity;
-            const k = MAX_HISTORY_SAMPLES - i - 1;
-            for (let j=0; j < activity.length; ++j) {
-                png.data.writeUInt32BE((HEATMAP_COLOR << 8) + Math.round(activity[j] * 255), 4*(MAX_HISTORY_SAMPLES * ((num_neurons - j - 1 + num_neurons / 2) % num_neurons) + k))
-            }
+    const data = useRef<HeatmapData>(null);
+    if (!data.current || props.state.time < data.current.time) {
+        data.current = initializeHeatmap(CHUNK_COUNT, MAX_HISTORY_SAMPLES, props.state.neurons);
+    }
+
+    if (props.state.time > data.current!.time) {
+        data.current!.time = props.state.time;
+        // Time has advanced; record new data.
+        const color = HEATMAP_COLOR;
+        const i = data.current!.headColumn;
+        const width = data.current!.head.width;
+        const height = data.current!.head.height;
+        const activity = props.history[props.history.length - 1].activity;
+        for (let j=0; j < props.state.neurons; ++j) {
+            data.current!.head.data.writeInt32BE(
+                (color << 8) + Math.round(activity[j] * 0xff),
+                4*(width * ((height - j - 1 + height / 2) % height) + i));
+        }
+        data.current!.headColumn++;
+
+        if (data.current!.headColumn == width) {
+            data.current!.headColumn = 0;
+
+            data.current!.completedChunks.splice(0, 1);
+            data.current!.completedChunks.push(PNG.sync.write(data.current!.head).toString("base64"));
+            data.current.head = new PNG({ height: props.state.neurons, width: data.current!.chunkWidth })
         }
     }
-    const img = PNG.sync.write(png).toString("base64");
 
-    return <image imageRendering="pixelated" preserveAspectRatio="none" x={props.xExtent[0]} y={props.yExtent[1]} width={props.xExtent[1] - props.xExtent[0]} height={props.yExtent[0] - props.yExtent[1]} xlinkHref={`data:image/png;base64,${img}`}/>;
+    const headChunk = PNG.sync.write(data.current!.head).toString("base64");
+    const chunkWidth = (props.xExtent[1] - props.xExtent[0]) / CHUNK_COUNT;
+    const headOffset = chunkWidth / data.current!.chunkWidth * data.current!.headColumn;
+    return (
+        <>
+            <defs>
+                <clipPath id="heatmap-area">
+                    <rect
+                        x={props.xExtent[0]}
+                        y={props.yExtent[1]}
+                        width={props.xExtent[1] - props.xExtent[0]}
+                        height={props.yExtent[0] - props.yExtent[1]}
+                    />
+                </clipPath>
+            </defs>
+            {
+                data.current.completedChunks.map((chunk, i) =>
+                    <image key={i}
+                        imageRendering="pixelated"
+                        preserveAspectRatio="none"
+                        x={props.xExtent[0] + i * chunkWidth - headOffset}
+                        y={props.yExtent[1]}
+                        width={chunkWidth}
+                        height={props.yExtent[0] - props.yExtent[1]}
+                        xlinkHref={`data:image/png;base64,${chunk}`}
+                        clipPath="url(#heatmap-area)"/>
+                )
+            }
+            <image key={CHUNK_COUNT}
+                imageRendering="pixelated"
+                preserveAspectRatio="none"
+                x={props.xExtent[0] + CHUNK_COUNT * chunkWidth - headOffset}
+                y={props.yExtent[1]}
+                width={chunkWidth}
+                height={props.yExtent[0] - props.yExtent[1]}
+                xlinkHref={`data:image/png;base64,${headChunk}`}/>
+        </>
+    );
 }
